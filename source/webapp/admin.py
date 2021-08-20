c0cb839185a1a1976ab6503bdc1fad1988cb928a
Gosha <begoshaaaa@gmail.com>
2021-08-20 21:10:10 +0600

    models are registered

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from webapp.models import (
+    Image,
+    Advert
+)
 
-# Register your models here.
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'phone_number', 'created_at']
+    list_filter = ['author']
+    search_fields = ['author']
+    fields = ['id', 'author', 'description', 'phone_number', 'created_at']
+    readonly_fields = ['id', 'created_at']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'advert', 'image']
+    list_filter = ['advert']
+    search_fields = ['advert']
+    fields = ['id', 'image', 'advert']
+    readonly_fields = ['id']
